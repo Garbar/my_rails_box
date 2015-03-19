38b3e605e114bec9781b776cd3a5af41412409e4
Oksana <lorelsblog@gmail.com>
2015-03-19 13:14:44 +0200

    clear cookbooks

--- site-cookbooks/postinstall/attributes/default.rb
+++
@@ -1,9 +0,0 @@
-default['user']['name']    = 'deploy'
-default['app']['name']    = 'rails20'
-default['domain']['name']    = ''
-default['domain']['ip']    = '128.199.62.181'
-
-default['nginx']['path']   = '/etc/nginx'
-default['nginx']['conf']['worker_processes']   = 2
-default['app']['password'] = 'super_secret'
-
